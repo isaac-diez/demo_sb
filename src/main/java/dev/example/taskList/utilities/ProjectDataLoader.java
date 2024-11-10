5fe610a4d16900130b5f991756a7af215d46b61e
isaac <isaac.diez@proton.me>
2024-11-10 17:06:09 +0100

    #1 implemented ProjectDataLoader and TagDataLoader. to-do: - project FK in task h2 table - task-tag table in h2

--- src/main/java/dev/example/taskList/utilities/ProjectDataLoader.java
+++ src/main/java/dev/example/taskList/utilities/ProjectDataLoader.java
@@ -1,4 +1,42 @@
 package dev.example.taskList.utilities;
 
+import com.github.javafaker.Faker;
+import dev.example.taskList.model.Project;
+import dev.example.taskList.repository.ProjectRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+import java.util.UUID;
+
+@Component
 public class ProjectDataLoader {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @PostConstruct
+    public void createFakeProjects() {
+
+        if(projectRepository.count() == 0) {
+            System.out.println(" 0 records at the database found");
+            Faker faker = new Faker(new Locale("en-US"));
+
+            int qty = 5;
+            for(int i = 0; i < qty; i++) {
+                Project project = new Project();
+                project.setId((long) UUID.randomUUID().hashCode());
+                project.setName(faker.animal().name());
+                project.setDescription(faker.lorem().sentence());
+
+                projectRepository.save(project);
+            }
+            System.out.println(qty + " fake Project records created");
+        }
+
+    }
+
+
+
 }
--- src/main/java/dev/example/taskList/utilities/TagDataLoader.java
+++ src/main/java/dev/example/taskList/utilities/TagDataLoader.java
@@ -2,7 +2,10 @@ package dev.example.taskList.utilities;
 
 import com.github.javafaker.Faker;
 import dev.example.taskList.model.AppUser;
+import dev.example.taskList.model.Tag;
 import dev.example.taskList.repository.AppUserRepository;
+import dev.example.taskList.repository.TagRepository;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import java.util.Locale;
@@ -12,9 +15,26 @@ import java.util.UUID;
 public class TagDataLoader {
 
     @Autowired
-    private AppUserRepository appUserRepository;
+    private TagRepository tagRepository;
 
-    public void createFakeTagDataLoader() {
+    @PostConstruct
+    public void createFakeTags() {
+
+        if (tagRepository.count() == 0) {
+            System.out.println(" 0 records at the database found");
+            Faker faker = new Faker(new Locale("en-US"));
+
+            int qty = 10;
+            for (int i = 0; i < qty; i++) {
+                Tag tag = new Tag();
+
+                Long id = (long) UUID.randomUUID().hashCode();
+                String name = faker.superhero().name();
+
+                tagRepository.save(tag);
+            }
+            System.out.println(qty + " fake Tag records created");
+        }
     }
 
 }
